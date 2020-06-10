ceb4e6ad3f66b6e4ec52740be6e10dc9658f04fc
Semenov <semenov@skeeks.com>
2020-06-10 13:02:58 +0300

    dev

--- src/config/web.php
+++ src/config/web.php
@@ -8,22 +8,8 @@ return [
                         'items' => [
                             [
                                 "name"  => ['skeeks/logdb/app', 'Logs'],
+                                "url"   => ["logDbTarget/admin-log-db-target"],
                                 "image" => ['\skeeks\cms\logDbTarget\assets\LogDbTargetAsset', 'icons/log.png'],
-
-                                'items' => [
-                                    [
-                                        "name"  => ['skeeks/logdb/app', 'Logs'],
-                                        "url"   => ["logDbTarget/admin-log-db-target"],
-                                        "image" => ['\skeeks\cms\logDbTarget\assets\LogDbTargetAsset', 'icons/log.png'],
-                                    ],
-
-                                    /*[
-                                        "name"  => ['skeeks/logdb/app', 'Settings'],
-                                        "url"   => ["cms/admin-settings", "component" => 'skeeks\cms\LogDbTarget\components\LogDbTargetSettings'],
-                                        "image" => ['\skeeks\cms\assets\CmsAsset', 'images/icons/settings-big.png'],
-                                    ],*/
-
-                                ],
                             ],
                         ],
                     ],
